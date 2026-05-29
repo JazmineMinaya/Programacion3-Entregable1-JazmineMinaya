a9ad269c810a76faf8eb233db1642ffdfa06a73b
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-29 13:02:49 -0400

    Creación del método para mostrar el menú y el método del main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
 public class Main {
-    
+    public static void mostrarMenu() {
+        System.out.println("\n====================================");
+        System.out.println("    SISTEMA DE GESTION ACADEMICA");
+        System.out.println("====================================");
+        System.out.println("\n1. Registrar estudiante");
+        System.out.println("2. Registrar profesor");
+        System.out.println("3. Registrar materia");
+        System.out.println("4. Asignar materia a estudiante");
+        System.out.println("5. Registrar calificacion");
+        System.out.println("6. Buscar estudiante");
+        System.out.println("7. Mostrar estudiantes");
+        System.out.println("8. Mostrar materias");
+        System.out.println("9. Mostrar reporte de promedios");
+        System.out.println("10. Salir");
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
