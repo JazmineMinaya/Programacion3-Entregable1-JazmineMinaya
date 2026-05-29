bc4fef9825654e23635f75ab35740b8a142cfad6
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-29 14:25:05 -0400

    Declaración de atributos y creación de constructores, getters y setters en la clase Materia

--- src/Materia.java
+++ src/Materia.java
@@ -1,3 +1,37 @@
 public class Materia {
-    
+    private String codigoMateria;
+    private String nombreMateria;
+    private int cantidadCreditos;
+
+    public Materia() {
+        setCodigoMateria("");
+        setNombreMateria("");
+        setCantidadCreditos(0);
+    }
+
+    public Materia(String codigoMateria, String nombreMateria, int cantidadCreditos) {
+        setCodigoMateria(codigoMateria);
+        setNombreMateria(nombreMateria);
+        setCantidadCreditos(cantidadCreditos);
+    }
+
+    public String getCodigoMateria() {
+        return codigoMateria;
+    }
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+    public int getCantidadCreditos() {
+        return cantidadCreditos;
+    }
+
+    public void setCodigoMateria(String codigoMateria) {
+        this.codigoMateria = codigoMateria;
+    }
+    public void setNombreMateria(String nombreMateria) {
+        this.nombreMateria = nombreMateria;
+    }
+    public void setCantidadCreditos(int cantidadCreditos) {
+        this.cantidadCreditos = cantidadCreditos;
+    }
 }
