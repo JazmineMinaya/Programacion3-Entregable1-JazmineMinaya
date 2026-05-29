b2a13e1e99134947db36b8d5bed86f2d49cb4a53
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-05-29 14:12:55 -0400

    Declaración de atributos y creación de constructores, getters y setters en la clase Profesor

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,3 +1,32 @@
 public class Profesor {
-    
+    private String codigoProfesor;
+    private String nombreProfesor;
+    private String apellidoProfesor;
+    private String especialidad;
+
+    public String getCodigoProfesor() {
+        return codigoProfesor;
+    }
+    public String getNombreProfesor() {
+        return nombreProfesor;
+    }
+    public String getApellidoProfesor() {
+        return apellidoProfesor;
+    }
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setCodigoProfesor(String codigoProfesor) {
+        this.codigoProfesor = codigoProfesor;
+    }
+    public void setNombreProfesor(String nombreProfesor) {
+        this.nombreProfesor = nombreProfesor;
+    }
+    public void setApellidoProfesor(String apellidoProfesor) {
+        this.apellidoProfesor = apellidoProfesor;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
 }
